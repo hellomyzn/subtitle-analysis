64ef55f2292faef1c1c15b9c48a13f90f6e7f713
hellomyzn <em711th@icloud.com>
2021-09-30 14:37:17 +0900

    read file

--- codes/count_words.py
+++ codes/count_words.py
@@ -1,17 +1,11 @@
 import sys
 
 def main():
-
-    # get arguments
     args = sys.argv
-    try:
-        args[1]
-    except:
-        print(str.upper("***   choose file which you want to try   ***"))
-    
-    
-    # Read files
-    # read_file()
+
+    file_path = get_file_path(args)
+
+    read_file(file_path)
 
     # extract sentence
 
@@ -19,7 +13,22 @@ def main():
     print("done")
 
 
-# def read_file()
+def get_file_path(args):
+    try:
+        return args[1]
+    except:
+        print(str.upper("***   choose file which you want to try   ***"))
+        exit()
+
+
+def read_file(file_path):
+    try:
+        with open(file_path) as f:
+            for line in f:
+                print(line)
+    except:
+        print(str.upper("***   wrong file path   ***"))
+        exit()
 
 
 
