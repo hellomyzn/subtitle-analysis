58100314c9a03d93f5a90c90c7fa16a265565047
hellomyzn <em711th@icloud.com>
2021-09-30 14:26:16 +0900

    add args

---
+++ codes/count_words.py
@@ -0,0 +1,35 @@
+import sys
+
+def main():
+
+    # get arguments
+    args = sys.argv
+    try:
+        args[1]
+    except:
+        print(str.upper("***   choose file which you want to try   ***"))
+    
+    
+    # Read files
+    # read_file()
+
+    # extract sentence
+
+    # separate words
+    print("done")
+
+
+# def read_file()
+
+
+
+# def extract_subtitle()
+
+
+# def create_words_list()
+
+
+    
+
+if __name__ == "__main__":
+    main()
