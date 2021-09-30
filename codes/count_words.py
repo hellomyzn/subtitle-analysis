468c574bcb490f6be5d39951e7be344caac11b20
hellomyzn <em711th@icloud.com>
2021-09-30 17:17:35 +0900

    remove noise

--- codes/count_words.py
+++ codes/count_words.py
@@ -1,18 +1,18 @@
 import sys
 
+import collections
+
+
 def main():
     args = sys.argv
+    words = []
+    words_without_noise = []
+    noise = []
 
     file_path = get_file_path(args)
-
-    sentences = read_file(file_path)
-
-    extract_subtitle(sentences)
-
-    # separate words
-    print("done")
-    
-
+    extract_words(file_path, words)
+    remove_noise(words, words_without_noise, noise)
+    count_words(words_without_noise)
 
 def get_file_path(args):
     try:
@@ -22,26 +22,39 @@ def get_file_path(args):
         exit()
 
 
-def read_file(file_path):
+def extract_words(file_path, words):
     try:
         with open(file_path) as f:
-            s = f.read()
-            return s.splitlines()
-
+            for line in f:
+                words.extend(line.split())
     except:
         print(str.upper("***   wrong file path   ***"))
         exit()
 
 
+def remove_noise(words, words_without_noise, noise):
+    words = [word.replace("'", '') for word in words]
+    words = [word.replace('"', '') for word in words]
+    words = [word.replace(":", '') for word in words]
+    words = [word.replace(",", '') for word in words]
+    words = [word.replace(".", '') for word in words]
+    words = [word.replace("?", '') for word in words]
+    words = [word.replace("!", '') for word in words]
+    words = [word.replace("-", '') for word in words]
+    words = [word.replace(">", '') for word in words]
+    words = [word.replace("[", '') for word in words]
+    words = [word.replace("]", '') for word in words]
 
-def extract_subtitle(sentences):
-    print(sentences)
-
-
-# def create_words_list()
-
-
-    
+    for word in words:
+        # remove except Alphabet
+        if word.isalpha():
+            words_without_noise.append(str.lower(word))
+        else:
+            noise.append(word)
+        
+def count_words(words_without_noise):
+    c = collections.Counter(words_without_noise)
+    print(c.items())
 
 if __name__ == "__main__":
     main()
