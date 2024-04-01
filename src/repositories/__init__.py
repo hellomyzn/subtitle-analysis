834a9e6778d6a44330f77376ac83a3b9c453e016
hellomyzn <em711th@icloud.com>
2024-04-01 18:13:20 +0900

    add: init

--- src/repositories/__init__.py
+++ src/repositories/__init__.py
@@ -1,3 +1,6 @@
 """repositories"""
 
 from .model_adapter import ModelAdapter
+from .base_repository import BaseRepositoryInterface
+from .csv_base_repository import CsvBaseRepository
+from .gss_base_repository import GssBaseRepository
